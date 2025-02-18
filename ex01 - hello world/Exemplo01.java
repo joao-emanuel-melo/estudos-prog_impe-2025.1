51c23d2c67bcd82200c8d345a1a23d7568f7cf46
João Emanuel <143614248+joao-emanuel-melo@users.noreply.github.com>
2025-02-18 23:46:14 +0000

    Programa01 - exemplo de funções

---
+++ ex01 - hello world/Exemplo01.java
@@ -0,0 +1,10 @@
+public class Exemplo01 {
+    public static void main(String[] args){
+        System.out.println("Olá Turma!");
+        System.out.println("O dobro de 10 é " + dobro(10));
+    }
+
+    public static int dobro(int x){
+        return 2 * x;
+    }
+}
